e63b6194855082f88bb5db66fb19fda00cc11d16
yetongxue <yexyog@qq.com>
2023-02-27 21:49:23 +0800

    修复采用djongo数据库引擎无法删除选中对象问题

--- simpleui/admin.py
+++ simpleui/admin.py
@@ -1,13 +1,101 @@
 import json
+import numpy as np
 import traceback
-from functools import update_wrapper
-from django.contrib import admin
+from django.contrib import admin, admin, messages
+from django.contrib.admin import helpers
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
 from django.db.models import Q
-from django.http import JsonResponse
+from django.http import JsonResponse, JsonResponse, HttpResponseNotAllowed, HttpResponseRedirect
+from django.http.response import HttpResponseBase
 from django.urls import path
+from django.utils.log import log_response
+from django.utils.translation import gettext as _
+from djongo.models import ObjectIdField
+from functools import update_wrapper, wraps
 
 
-class AjaxAdmin(admin.ModelAdmin):
+class MyAdmin(admin.ModelAdmin):
+
+    def response_action(self, request, queryset):
+        """
+        Handle an admin action. This is called if a request is POSTed to the
+        changelist; it returns an HttpResponse if the action was handled, and
+        None otherwise.
+        """
+
+        # There can be multiple action forms on the page (at the top
+        # and bottom of the change list, for example). Get the action
+        # whose button was pushed.
+        try:
+            action_index = int(request.POST.get("index", 0))
+        except ValueError:
+            action_index = 0
+
+        # Construct the action form.
+        data = request.POST.copy()
+        data.pop(helpers.ACTION_CHECKBOX_NAME, None)
+        data.pop("index", None)
+
+        # Use the action whose button was pushed
+        try:
+            data.update({"action": data.getlist("action")[action_index]})
+        except IndexError:
+            # If we didn't get an action from the chosen form that's invalid
+            # POST data, so by deleting action it'll fail the validation check
+            # below. So no need to do anything here
+            pass
+
+        action_form = self.action_form(data, auto_id=None)
+        action_form.fields["action"].choices = self.get_action_choices(request)
+
+        # If the form's valid we can handle the action.
+        if action_form.is_valid():
+            action = action_form.cleaned_data["action"]
+            select_across = action_form.cleaned_data["select_across"]
+            func = self.get_actions(request)[action][0]
+
+            # Get the list of selected PKs. If nothing's selected, we can't
+            # perform an action on it, so bail. Except we want to perform
+            # the action explicitly on all objects.
+            selected = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+
+            # 修复数据库引擎djongo无法删除选中对象问题
+            try:
+                from bson import ObjectId
+                selected = [ObjectId(item) for item in selected]
+            except Exception as e:
+                pass
+
+            if not selected and not select_across:
+                # Reminder that something needs to be selected or nothing will happen
+                msg = _(
+                    "Items must be selected in order to perform "
+                    "actions on them. No items have been changed."
+                )
+                self.message_user(request, msg, messages.WARNING)
+                return None
+
+            if not select_across:
+                # Perform the action only on the selected objects
+                queryset = queryset.filter(pk__in=selected)
+
+            response = func(self, request, queryset)
+
+            # Actions may return an HttpResponse-like object, which will be
+            # used as the response from the POST. If not, we'll be a good
+            # little HTTP citizen and redirect back to the changelist page.
+            if isinstance(response, HttpResponseBase):
+                return response
+            else:
+                return HttpResponseRedirect(request.get_full_path())
+        else:
+            msg = _("No action selected.")
+            self.message_user(request, msg, messages.WARNING)
+            return None
+
+
+class AjaxAdmin(MyAdmin):
     """
     This class is used to add ajax functionality to the admin interface.
     """
